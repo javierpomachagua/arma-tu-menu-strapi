78c633364601d5c6e2d7bcd87f510b0464428c21
Javier Pomachagua <javierpp0303@gmail.com>
2020-10-25 12:31:22 -0500

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b274046826153b8d0b0f9059707e6d18'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a28f13a1-c424-4ef3-b010-25232c9e8cf3'
+};
